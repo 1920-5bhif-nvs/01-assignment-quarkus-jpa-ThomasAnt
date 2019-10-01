aff5d65cf5be19d9abc4d66785c8eaa47c528de6
Thomas <thomas.antensteiner@hotmail.com>
2019-10-01 08:28:07 +0200

    initial commit

---
+++ src/main/java/com/codewithmansoor/GreetingResource.java
@@ -0,0 +1,32 @@
+package com.codewithmansoor;
+
+import javax.inject.Inject;
+import javax.print.attribute.standard.Media;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import io.agroal.api.AgroalDataSource;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @Inject
+    GreetingService service;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public String greeting(@PathParam("name") String name)
+    {
+        return service.greeting(name);
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello (){
+        return "hello\n";
+    }
+}
